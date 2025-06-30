6b28f6e95cbcf22bb8abd0eb8d7e652767312bcc
ProgramMinerTech <ao.tumbaflores@gmail.com>
2025-06-30 17:05:34 -0500

    feat: Print & Type in Python (.py)

--- 00_HelloWorld.py
+++ 00_HelloWorld.py
@@ -6,5 +6,11 @@ Esto es un comentario
 # Aprenderemos a imprimir
 
 print("Hello,")
-print("My name is Aaron,")
-print("My lastname is Tumba.")
+print(type("My name is Aaron,"))
+print(type(5))
+print(type(5.55))
+print(type(True))
+print(type(5 + 2j))
+print(type([6, 5, 7]))
+print(type((5, 6, 7)))
+print(type({5, 7, 9, 10}))
